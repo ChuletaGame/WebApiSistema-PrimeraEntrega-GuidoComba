ad4c750f266a0a144d98996054866afb59a6392e
ChuletaGame <136846887+ChuletaGame@users.noreply.github.com>
2024-02-16 00:19:12 -0300

    All service modif

--- WebApiSistema-PrimeraEntrega-GuidoComba/Service/ProductoData.cs
+++ WebApiSistema-PrimeraEntrega-GuidoComba/Service/ProductoData.cs
@@ -10,77 +10,64 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApiSistema_PrimeraEntrega_GuidoComba.Service
 {
-    public static class ProductoData
+    public class ProductoData
     {
-        public static Producto ObtenerProducto(int id)
+        private CoderContext context;
+        public ProductoData(CoderContext coderContext)
         {
-            using (CoderContext context = new CoderContext())
-            {
-                Producto productoBuscado = context.Productos.Where(u => u.Id == id).FirstOrDefault();
-
-                return productoBuscado;
-            }
-
-
+            this.context = coderContext;
         }
 
-        public static List<Producto> ListarProductos()
+        public  Producto ObtenerProducto(int id)
         {
-            using (CoderContext context = new CoderContext())
-            {
-                List<Producto> productos = context.Productos.ToList();
+            Producto productoBuscado = context.Productos.Where(u => u.Id == id).FirstOrDefault();
 
-                return productos;
-            }
+            return productoBuscado;
         }
 
-        public static bool CrearProducto(Producto producto)
+        public  List<Producto> ListarProductos()
         {
-            using (CoderContext context = new CoderContext())
+            List<Producto> productos = context.Productos.ToList();
+
+            return productos;
+        }
+
+        public  bool CrearProducto(Producto producto)
+        {
+            context.Productos.Add(producto);
+            context.SaveChanges();
+
+            return true;
+        }
+
+        public  bool ModificarProducto(Producto producto, int id)
+        {
+            Producto ProductoBuscado = context.Productos.Where(p => p.Id == id).FirstOrDefault();
+
+            ProductoBuscado.Descripciones = producto.Descripciones;
+            ProductoBuscado.Costo = producto.Costo;
+            ProductoBuscado.PrecioVenta = producto.PrecioVenta;
+            ProductoBuscado.Stock = producto.Stock;
+            ProductoBuscado.IdUsuario = producto.IdUsuario;
+
+            context.Productos.Update(ProductoBuscado);
+            context.SaveChanges();
+
+            return true;
+        }
+
+        public  bool EliminarProducto(int id)
+        {
+            Producto productoABorrado = context.Productos.Include(p => p.ProductoVendidos).Where(p => p.Id == id).FirstOrDefault();
+
+            if (productoABorrado is not null)
             {
-                context.Productos.Add(producto);
+                context.Productos.Remove(productoABorrado);
                 context.SaveChanges();
-
                 return true;
             }
-        }
 
-        public static bool ModificarProducto(Producto producto, int id)
-        {
-            using (CoderContext context = new CoderContext())
-            {
-                Producto ProductoBuscado = context.Productos.Where(p => p.Id == id).FirstOrDefault();
-
-                
-                ProductoBuscado.Descripciones = producto.Descripciones;
-                ProductoBuscado.Costo = producto.Costo;
-                ProductoBuscado.PrecioVenta = producto.PrecioVenta;
-                ProductoBuscado.Stock = producto.Stock;
-                ProductoBuscado.IdUsuario = producto.IdUsuario;
-
-                context.Productos.Update(ProductoBuscado);
-                context.SaveChanges();
-
-                return true;
-            }
-        }
-
-        public static bool EliminarProducto(int id)
-        {
-            using (CoderContext context = new CoderContext())
-            {
-                Producto productoABorrado = context.Productos.Include(p => p.ProductoVendidos).Where(p => p.Id == id).FirstOrDefault();
-
-                if (productoABorrado is not null)
-                {
-                    context.Productos.Remove(productoABorrado);
-                    context.SaveChanges();
-                    return true;
-                }
-
-                return false;
-            }
-
+            return false;
         }
 
     }
--- WebApiSistema-PrimeraEntrega-GuidoComba/Service/ProductoVendidoData.cs
+++ WebApiSistema-PrimeraEntrega-GuidoComba/Service/ProductoVendidoData.cs
@@ -8,75 +8,62 @@ using System.Threading.Tasks;
 
 namespace WebApiSistema_PrimeraEntrega_GuidoComba.Service
 {
-    public static class ProductoVendidoData
+    public class ProductoVendidoData
     {
-
-        public static ProductoVendido ObtenerProductoVendido (int id)
+        private CoderContext context;
+        public ProductoVendidoData(CoderContext coderContext)
         {
-            using (CoderContext context = new CoderContext())
-            {
-                ProductoVendido productoBuscadoVendido = context.ProductoVendidos.Where(u => u.Id == id).FirstOrDefault();
-
-                return productoBuscadoVendido;
-            }
-
+            this.context = coderContext;
         }
 
-        public static List<ProductoVendido> ListarProductosVendidos()
+        public  ProductoVendido ObtenerProductoVendido (int id)
         {
-            using (CoderContext context = new CoderContext())
-            {
-                List<ProductoVendido> productoVendido = context.ProductoVendidos.ToList();
-
-                return productoVendido;
-            }
+            ProductoVendido productoBuscadoVendido = context.ProductoVendidos.Where(u => u.Id == id).FirstOrDefault();
+            
+            return productoBuscadoVendido;
         }
 
-        public static bool CrearProductoVendido(ProductoVendido productoVendido)
+        public  List<ProductoVendido> ListarProductosVendidos()
         {
-            using (CoderContext context = new CoderContext())
+            List<ProductoVendido> productoVendido = context.ProductoVendidos.ToList();
+
+            return productoVendido;
+        }
+
+        public  bool CrearProductoVendido(ProductoVendido productoVendido)
+        {
+            context.ProductoVendidos.Add(productoVendido);
+            context.SaveChanges();
+
+            return true;
+        }
+
+        public  bool ModificarProductoVendido(ProductoVendido productoVendido, int id)
+        {
+            ProductoVendido ProductoVendidoBuscado = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
+
+            ProductoVendidoBuscado.Stock = productoVendido.Stock;
+            ProductoVendidoBuscado.IdProducto = productoVendido.IdProducto;
+            ProductoVendidoBuscado.IdVenta = productoVendido.IdVenta;
+
+            context.ProductoVendidos.Add(ProductoVendidoBuscado);
+            context.SaveChanges();
+
+            return true;
+        }
+
+        public  bool EliminarProductoVendido(int id)
+        {
+            ProductoVendido productoABorradoVendido = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
+
+            if (productoABorradoVendido is not null)
             {
-                context.ProductoVendidos.Add(productoVendido);
+                context.ProductoVendidos.Remove(productoABorradoVendido);
                 context.SaveChanges();
-
                 return true;
             }
-        }
 
-        public static bool ModificarProductoVendido(ProductoVendido productoVendido, int id)
-        {
-            using (CoderContext context = new CoderContext())
-            {
-                ProductoVendido ProductoVendidoBuscado = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
-
-
-                ProductoVendidoBuscado.Stock = productoVendido.Stock;
-                ProductoVendidoBuscado.IdProducto = productoVendido.IdProducto;
-                ProductoVendidoBuscado.IdVenta = productoVendido.IdVenta;
-
-                context.ProductoVendidos.Add(ProductoVendidoBuscado);
-                context.SaveChanges();
-
-                return true;
-            }
-        }
-
-        public static bool EliminarProductoVendido(int id)
-        {
-            using (CoderContext context = new CoderContext())
-            {
-                ProductoVendido productoABorradoVendido = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
-
-                if (productoABorradoVendido is not null)
-                {
-                    context.ProductoVendidos.Remove(productoABorradoVendido);
-                    context.SaveChanges();
-                    return true;
-                }
-
-                return false;
-            }
-
+            return false;
         }
     }
 }
